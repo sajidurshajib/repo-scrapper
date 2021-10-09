822ccbe4e6a872a028e46873108784d74458d025
Sajidur Rahman <sajidur.inbox@gmail.com>
2021-10-09 10:12:53 +0600

    repo link and csv output added

--- scrapper.js
+++ scrapper.js
@@ -1,73 +1,91 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
-
-let arr = []
+const user = "sajidurshajib";
+let arr = [];
 
 const scrapper = async () => {
-    const url = 'https://github.com/sajidurshajib?tab=repositories'
+    const url = `https://github.com/${user}?tab=repositories`;
 
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, {
-        waitUntil: 'networkidle2',
+        waitUntil: "networkidle2",
     });
-  
+
     //My code
     const test = await page.evaluate(() => {
         const repo = Array.from(
-            document.querySelectorAll('.wb-break-all > a'), 
-                element => element
-                        .textContent
-                        .replace(/\s+/g, ' ')
-                        .trim()
-            )
+            document.querySelectorAll(".wb-break-all > a"),
+            (element) => {
+                return {
+                    repoName: element.textContent.replace(/\s+/g, " ").trim(),
+                    repoLink: element.href,
+                };
+            }
+        );
 
         return {
-            repo
+            repo,
         };
     });
-    
+
     //1st push
-    arr.push(...test.repo)
+    arr.push(...test.repo);
 
-
-    while(await page.$('.BtnGroup > .BtnGroup-item:nth-child(2)')){
-        await page.click('.BtnGroup > .BtnGroup-item:nth-child(2)')
+    while (await page.$(".BtnGroup > .BtnGroup-item:nth-child(2)")) {
+        await page.click(".BtnGroup > .BtnGroup-item:nth-child(2)");
 
         await page.goto(page.url(), {
-            waitUntil: 'networkidle2',
+            waitUntil: "networkidle2",
         });
         const again = await page.evaluate(() => {
             const repo = Array.from(
-                document.querySelectorAll('.wb-break-all > a'), 
-                    element => element
-                            .textContent
-                            .replace(/\s+/g, ' ')
-                            .trim()
-                )
-    
+                document.querySelectorAll(".wb-break-all > a"),
+                (element) => {
+                    return {
+                        repoName: element.textContent
+                            .replace(/\s+/g, " ")
+                            .trim(),
+                        repoLink: element.href,
+                    };
+                }
+            );
+
             return {
-                repo
+                repo,
             };
         });
 
-
-
         //2nd push
-        arr.push(...again.repo)
+        arr.push(...again.repo);
 
         //loop break for disabled button
-        const is_disabled = await page.evaluate(() => document.querySelector('button[disabled]') !== null)
-        if(is_disabled===true){
-            break
+        const is_disabled = await page.evaluate(
+            () => document.querySelector("button[disabled]") !== null
+        );
+        if (is_disabled === true) {
+            break;
         }
-        
     }
 
-    console.log(arr)
+    console.log(arr);
+
+    //Write csv
+    const csvWriter = createCsvWriter({
+        path: "RepoList.csv",
+        header: [
+            { id: "repoName", title: "RepoName" },
+            { id: "repoLink", title: "RepoLink" },
+        ],
+    });
+
+    csvWriter.writeRecords(arr).then(() => {
+        console.log("Your csv is ready...");
+    });
+
+    //Close headless browser
     await browser.close();
-}
+};
 
-scrapper()
-
-
+scrapper();
